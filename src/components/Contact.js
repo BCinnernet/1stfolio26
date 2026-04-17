0f1f1198724253999588cbac267d85be8bba25cf
Ejuan <becausetheinnernet@gmail.com>
2026-04-17 11:15:49 -0500

    Shrink contact card social labels to 12px, fix SVG size scaling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -86,7 +86,7 @@ const Contact = () => {
                       <span>Instagram</span>
                     </a>
                     <a href="https://bsky.app/profile/becausetheinnernet.com" target="_blank" rel="noreferrer" aria-label="Bluesky">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 530" width="22" height="22" fill="currentColor">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 530" fill="currentColor">
                         <path d="M300 266c0 0-92-178-208-178-62 0-92 51-92 104 0 112 162 156 162 156s-55 40-55 88c0 51 46 94 104 94 58 0 89-40 89-40s31 40 89 40c58 0 104-43 104-94 0-48-55-88-55-88s162-44 162-156c0-53-30-104-92-104-116 0-208 178-208 178z" />
                       </svg>
                       <span>Bluesky</span>
