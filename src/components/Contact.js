56ae829235160a608fd4ad94f3851d39bb262554
Ejuan <becausetheinnernet@gmail.com>
2026-04-17 11:11:07 -0500

    Add Instagram/Bluesky labels to contact card socials

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -81,30 +81,15 @@ const Contact = () => {
                   </a>
 
                   <div className="contact-card-socials">
-                    <a
-                      href="https://www.instagram.com/ohhej"
-                      target="_blank"
-                      rel="noreferrer"
-                      aria-label="Instagram"
-                    >
+                    <a href="https://www.instagram.com/ohhej" target="_blank" rel="noreferrer" aria-label="Instagram">
                       <i className="fab fa-instagram" />
+                      <span>Instagram</span>
                     </a>
-
-                    <a
-                      href="https://bsky.app/profile/becausetheinnernet.com"
-                      target="_blank"
-                      rel="noreferrer"
-                      aria-label="Bluesky"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 600 530"
-                        width="22"
-                        height="22"
-                        fill="currentColor"
-                      >
+                    <a href="https://bsky.app/profile/becausetheinnernet.com" target="_blank" rel="noreferrer" aria-label="Bluesky">
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 530" width="22" height="22" fill="currentColor">
                         <path d="M300 266c0 0-92-178-208-178-62 0-92 51-92 104 0 112 162 156 162 156s-55 40-55 88c0 51 46 94 104 94 58 0 89-40 89-40s31 40 89 40c58 0 104-43 104-94 0-48-55-88-55-88s162-44 162-156c0-53-30-104-92-104-116 0-208 178-208 178z" />
                       </svg>
+                      <span>Bluesky</span>
                     </a>
                   </div>
 
