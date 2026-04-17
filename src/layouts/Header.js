311786b51b33ed3cb72cd96bf90a58203e635214
Ejuan <becausetheinnernet@gmail.com>
2026-04-17 10:43:39 -0500

    Update header socials: full names, larger font, muted color

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -48,7 +48,7 @@ const Header = ({ headerColor, isTransparent }) => {
                 aria-label="Instagram"
               >
                 <i className="fab fa-instagram" />
-                <span className="logo-social-label">IG</span>
+                <span className="logo-social-label">Instagram</span>
               </a>
               <a
                 href="https://bsky.app/profile/becausetheinnernet.com"
@@ -65,7 +65,7 @@ const Header = ({ headerColor, isTransparent }) => {
                 >
                   <path d="M300 266c0 0-92-178-208-178-62 0-92 51-92 104 0 112 162 156 162 156s-55 40-55 88c0 51 46 94 104 94 58 0 89-40 89-40s31 40 89 40c58 0 104-43 104-94 0-48-55-88-55-88s162-44 162-156c0-53-30-104-92-104-116 0-208 178-208 178z" />
                 </svg>
-                <span className="logo-social-label">BSKY</span>
+                <span className="logo-social-label">Bluesky</span>
               </a>
             </div>
 
