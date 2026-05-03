5c40628814e56d5786544a65861aec0be81863ab
Ejuan <becausetheinnernet@gmail.com>
2026-05-02 23:24:21 -0500

    Fix bold text: remove Poppins from _document.js font link

--- pages/_document.js
+++ pages/_document.js
@@ -41,7 +41,7 @@ export default function Document() {
         {/* Preconnect + load Google Fonts reliably on all devices including iOS */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Covered+By+Your+Grace&family=Poppins:wght@900&display=block" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Covered+By+Your+Grace&display=block" rel="stylesheet" />
       </Head>
       <body>
         <Main />
