ec648615e11f21f27ab2004e1b7ddec2b0c517ed
Ejuan <becausetheinnernet@gmail.com>
2026-05-05 13:50:18 -0500

    Update blurb: makes all the difference

--- pages/index.jsx
+++ pages/index.jsx
@@ -274,7 +274,7 @@ const Index3 = () => {
                   className="home-teaser-body sr"
                   style={{ "--sr-delay": "270ms" }}
                   segments={[
-                    { text: "I work across illustration, design, and motion/animation. I try to keep that childlike curiosity in my process. Having fun and making space for play makes a difference. I'm always open to exploring ideas and seeing what we can make happen.", italic: false },
+                    { text: "I work across illustration, design, and motion/animation. I try to keep that childlike curiosity in my process. Having fun and making space for play makes all the difference. I'm always open to exploring ideas and seeing what we can make happen.", italic: false },
                   ]}
                 />
 
