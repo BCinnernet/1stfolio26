db48208cc4aa9fcfb3dee5d36b6ddde05cbd7897
Ejuan <becausetheinnernet@gmail.com>
2026-05-08 15:50:14 -0500

    Add favicon size hints for Google

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,9 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
 
         <link
